08bc01f09610f84f589cb30ffc830e824165c3b9
Ryan Dahl <ry@tinyclouds.org>
2021-07-19 12:33:37 -0400

    z

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 addEventListener("fetch", (event) => {
   console.log("log message");
+  fetch("https://google.com/").then(r => {
+    console.log("got response");
+  });
   event.respondWith(new Response("world"));
 });
